73ff77b838af2ec2a2c53acf1e833488d8954373
rzldimam28 <hanjisung00>
2022-03-31 13:23:39 +0800

    adding files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Golang RESTful API
+
+### Simple RESTful API with Controller -> Service -> Repository Architecture
+
+### Spec:
+
+- web fr: httprouter
+- db: mysql
+- validation: go-playground/validator
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"belajar-golang-restful-api/app"
+	"belajar-golang-restful-api/controller"
+	"belajar-golang-restful-api/exception"
+	"belajar-golang-restful-api/helper"
+	"belajar-golang-restful-api/middleware"
+	"belajar-golang-restful-api/repository"
+	"belajar-golang-restful-api/service"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+
+	db := app.NewDB()
+	validate := validator.New()
+
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	router.PanicHandler = exception.ErrorHandler
+
+	server := http.Server{
+		Addr: "localhost:3000",
+		Handler: middleware.NewAuthMiddleware(router),
+	}
+
+	log.Println("Server is running...")
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+
+}
